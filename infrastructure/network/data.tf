6ac8913f8f608cf71bd10dfa8d36cb3557946230
Subham Datta <subhamdatta11@gmail.com>
2025-06-14 13:29:57 +0000

    updates with monitoring for vnet

--- infrastructure/network/data.tf
+++ infrastructure/network/data.tf
@@ -1,2 +1,6 @@
 data "azurerm_client_config" "current" {}
 
+data "azurerm_log_analytics_workspace" "this" {
+  name                = local.log_analytics_ws_name
+  resource_group_name = local.monitor_rsg_name
+}
