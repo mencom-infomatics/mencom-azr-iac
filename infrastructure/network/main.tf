1d24c8860edc04212a71102143a40f824a74c811
Subham Datta <subhamdatta11@gmail.com>
2025-06-14 12:58:01 +0000

    update

--- infrastructure/network/main.tf
+++ infrastructure/network/main.tf
@@ -1,12 +1,12 @@
-resource "azurerm_resource_group" "network" {
+resource "azurerm_resource_group" "this" {
   name     = local.network_rsg_name
   location = var.location
 }
 
-resource "azurerm_virtual_network" "virtual_network" {
+resource "azurerm_virtual_network" "this" {
   name                = local.virtual_network_name
-  location            = azurerm_resource_group.network.location
-  resource_group_name = azurerm_resource_group.network.name
+  location            = azurerm_resource_group.this.location
+  resource_group_name = azurerm_resource_group.this.name
   address_space       = var.vnet_address_spaces
 
   tags = {
